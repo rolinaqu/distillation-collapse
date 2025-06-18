5d555b58e312b5aa4a1fb70794fe492ec2667a21
Rolina Qu <rolinaqu@cardinal-rw02.ten.osc.edu>
2025-06-18 16:17:11 -0400

    modified premodel

--- DM+ours/pre_model.py
+++ DM+ours/pre_model.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 import time
 import copy
@@ -13,6 +13,7 @@ from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_s
 from torchvision.utils import save_image, make_grid
 import torch.nn.functional as F
 import torchvision
+import wandb
 from utils import get_premodel
 def main():
     parser = argparse.ArgumentParser(description='Parameter Processing')
@@ -24,7 +25,7 @@ def main():
     parser.add_argument('--num_premodels', type=int, default=100, help='path to save results')
     parser.add_argument('--pre_epochs', type=int, default=40, help='path to save results')
     # To decrease train time, we can set smaller "pre_epochs" and num_premodels""
-    parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
+    parser.add_argument('--lr_net', type=float, default=0.001, help='learning rate for updating network parameters')
     parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
     args = parser.parse_args()
     args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -34,24 +35,27 @@ def main():
     channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path)
     num_premodels=args.num_premodels
     pre_epochs=args.pre_epochs
-    print("running num_premodels")
+    print("Training " + str(num_premodels) + " premodels for " + str(pre_epochs) + " epochs.")
     for i in range(0,num_premodels):
-        print(i)
+        print("Training model number " + str(i))
         net = get_network(args.model, channel, num_classes, im_size).to(args.device) # get a random model
 
         optimizer = torch.optim.SGD(net.parameters(), lr=args.lr_net, momentum=0.9, weight_decay=0.0005)
         criterion = nn.CrossEntropyLoss().to(args.device)
         trainloader = torch.utils.data.DataLoader(dst_train, batch_size=args.batch_train, shuffle=True, num_workers=6)
         for ep in range(pre_epochs):
+            #print("Epoch number " + str(ep))
             loss_train, acc_train = epoch('train', trainloader, net, optimizer, criterion, args, aug = False)
             loss_test, acc_test = epoch('test', testloader, net, optimizer, criterion, args, aug = False)
+            print("Epoch number " + str(acc_test))
             if ep == pre_epochs -1:
-                print("Final epoch for Model {i}, Test Accuracy: "+str(acc_test))
+            	print("Final epoch for Model " + str(i) + ", Test Accuracy: "+str(acc_test))
         #save path
-        save_path="./DM_pre_dict"
+        #save_path="./DM_pre_dict"
+        save_path = args.save_path
         if not os.path.exists(save_path):
                 os.mkdir(save_path)
-        torch.save(net.state_dict(), save_path+"/buffer{}.pth".format(i))
+        torch.save(net.state_dict(), save_path+"/default001lr{}.pth".format(i))
         
 if __name__ == '__main__':
     main()
